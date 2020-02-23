b38f9ebe4ebd3b8ddd1dbe71810131ec084dd3f7
Terrier <rterrier@apc.in2p3.fr>
2020-02-23 18:55:21 +0100

    Use fixture for test

--- gammapy/cube/tests/test_model_estimator.py
+++ gammapy/cube/tests/test_model_estimator.py
@@ -8,22 +8,30 @@ from gammapy.spectrum import SpectrumDatasetOnOff
 from gammapy.modeling import Datasets
 from gammapy.modeling.models import PowerLawSpectralModel, SkyModel
 
-
-@requires_data()
-def test_model_estimator_simple():
+@pytest.fixture
+def crab_datasets_1d():
     filename = "$GAMMAPY_DATA/joint-crab/spectra/hess/pha_obs23523.fits"
     dataset = SpectrumDatasetOnOff.from_ogip_files(filename)
     datasets = Datasets([dataset])
 
+    return datasets
+
+@requires_data()
+def test_model_estimator_simple(crab_datasets_1d):
+    datasets = crab_datasets_1d
     PLmodel = PowerLawSpectralModel(amplitude="3e-11 cm-2s-1TeV-1", index=2.7)
-    dataset.models = SkyModel(spectral_model=PLmodel, name="Crab")
+    for dataset in datasets:
+        dataset.models = SkyModel(spectral_model=PLmodel, name="Crab")
 
     estimator = ModelEstimator()
 
-    result = estimator.run(datasets, PLmodel, steps=["errp-errn"])
+    result = estimator.run(datasets, PLmodel, steps=None)
 
     # First make sure that parameters are correctly set on the dataset.models object
-    assert_allclose(dataset.models[0].parameters['index'].value, 2.816696962376779)
-    assert_allclose(dataset.models[0].parameters.error('index'), 0.14957562779580705)
-    assert_allclose(dataset.models[0].parameters['amplitude'].value, 5.142843823441639e-11)
-    assert_allclose(dataset.models[0].parameters.error('amplitude'), 6.42467002840316e-12)
+    assert_allclose(datasets[0].models[0].parameters['index'].value, 2.816696962376779)
+    assert_allclose(datasets[0].models[0].parameters.error('index'), 0.14957562779580705)
+    assert_allclose(datasets[0].models[0].parameters['amplitude'].value, 5.142843823441639e-11)
+    assert_allclose(datasets[0].models[0].parameters.error('amplitude'), 6.42467002840316e-12)
+
+#@requires_data()
+#def test_model_estimator_ts():
